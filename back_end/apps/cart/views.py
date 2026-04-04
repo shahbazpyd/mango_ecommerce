00e235f2b7f317ed18cd48935031f60a23d468b8
shahbaz alam <sbzalam2024@gmail.com>
2026-04-04 19:18:38 +0530

    moved backend to root back_end

--- back_end/apps/cart/views.py
+++ back_end/apps/cart/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .models import Cart, CartItem
-from apps.products.models import Product
+from back_end.apps.products.models import Product
 
 class AddToCartView(APIView):
     permission_classes = [IsAuthenticated]
--- back_end/apps/orders/views.py
+++ back_end/apps/orders/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from apps.cart.models import Cart
+from back_end.apps.cart.models import Cart
 from .models import Order, OrderItem
 
 class CreateOrderView(APIView):
