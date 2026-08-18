00a957bd19d198ae6c56ab10403744e4e0c5b2fb
shahbaz alam <sbzalam2024@gmail.com>
2026-08-18 19:29:11 +0530

    feat: initialize Django project settings with database, storage, and app configurations

--- backend/settings.py
+++ backend/settings.py
@@ -159,4 +159,11 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
